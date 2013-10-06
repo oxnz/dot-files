3a231a19099550e7a6ea3df356ca339e47d8474e
oxnz <yunxinyi@gmail.com>
2013-10-06 13:00:34 +0800

    update dotfm.pl scan option

--- dotfm.pl
+++ dotfm.pl
@@ -1,95 +1,78 @@
 #!/usr/bin/perl
 package main;
+use vars (qw($VERSION),
+          qw($file, $option)
+         );
 
-our $VERSION = '0.01';
+$VERSION = '0.01';
 
 use strict;
 use warnings;
 use Getopt::Long;
+use File::Copy;
+use Data::Dumper;
 use Pod::Usage;
 
-sub VERSION_MESSAGE {
-	print "Version: $VERSION\n";
-}
 
-sub HELP_MESSAGE {
-	print "HELP MESSAGE\n";
-}
-
-sub help {
-	print "dotfm Dot File Manager\n";
+sub scan {
+	my $list = shift;
+	print "list=$list\n";
+	opendir(HOME, glob('~')) or die "Error in opening dir: ~\n";
+	open(LIST, ">$list") or closedir(HOME) and
+		die "Error in opening file: $list\n";
+	while (readdir(HOME)) {
+		next if $_ !~ m/^\./;
+		next if $_ =~ m/^(\.|\.\.)$/;
+		print LIST "$_\n";
+	}
+	close(LIST);
+	closedir(HOME);
+	return 0;
 }
 
 sub pack {
-	print "packing";
+	my $list = shift;
+	my $file = shift;
+	print "packing from $list to $file\n";
+	open(LIST, "<$list") or die "Error in opening file: $list\n";
+	my $Home = glob('~');
+	while(<LIST>) {
+		chomp;
+		my $f = $Home . "/" . $_;
+		print "file:$f\n";
+		copy($f, "/Users/xinyi/Desktop/tmp/$_") or warn "could not copy file: $f ($!)\n";
+   }
+
 }
 
-sub unpack() {
-	my $file = $_[0];
-	print "Unpacking [$file]\n";
+sub readconfig {
+	print "reading config\n";
+}
+
+sub compress {
+	print "compressing\n";
 }
 
 BEGIN {
-	my %opts = ();
-	print "hello\n";
-	print @ARGV;
-	print "World\n";
-	GetOptions( \%opts,
-		'help|h!',
-		'pack|p=s'		=> \%opts{'pack'},
-		'unpack|u=s'	=> \%opts['unpack'],
-		'config|c=s'	=> \%opts['config'],
-		'version|v!',
-	) or pod2usage(2);
-
-	pod2usage(1) if $opts{'help'};
-	&unpack("~/Desktop/x.tar.gz");
-
-
 	print "BEGIN\n";
 }
 
-END {
-	print "END\n";
+CHECK {
+	print "CHECK\n";
 }
 
-__END__
+INIT {
+	print "INIT\n";
+	my %opts = ();
+	GetOptions(\%opts, qw(help|h! version|v! scan|s! pack|p! list|l=s  unpack|u! file|f=s))
+		or die;
+	print Dumper(\%opts);
 
-=head1 NAME
+	&scan($opts{list}) if defined $opts{scan};
+	&pack($opts{list}, $opts{file}) if defined $opts{pack};
+}
 
-dotfm.pl - Dot file manager
+END {
+	print "process END\n";
+}
 
-=head1 SYNOPSIS
-
-dotfm.pl [options] -f <file>
-
-Options:
-
-	-h|help		print this help message and exit
-	-p|pack		package dot files under ~
-	-u|unpack	unpack dot files from specified package
-	-f|file		package file to pack or pack from
-
-=head1 OPTIONS
-
-=over 8
-
-=item B<-help>
-
-Print this help message and exits.
-
-=item B<-pack>
-
-package dot files under ~ to a specified package file
-
-=item B<-unpack>
-
-unpack dot files to ~ from a specified package file
-
-=back
-
-=head1 DESCRIPTION
-
-B<This program> will help to manage dot files under ~.
-
-=cut
