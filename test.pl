6e00f64c7440ab89fa22686559d308bf152da761
oxnz <yunxinyi@gmail.com>
2014-08-21 17:41:30 +0800

    update test.pl

--- test.pl
+++ test.pl
@@ -3,5 +3,11 @@
 use warnings;
 use strict;
 
-sub histop {
-}
+use Data::Dumper;
+use strict;
+
+my $del = qr/[^\\]\|/;
+my $str = 'a\|b|c|d';
+my @arr = split(/$del/, $str);
+print $str . "\n";
+print Dumper(@arr);
