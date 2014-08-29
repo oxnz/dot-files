c228d7b2fe9e80b927ebb7dff5d6a5ef8a2d7e57
oxnz <yunxinyi@gmail.com>
2014-08-29 19:54:36 +0800

    add awk 80 line

---
+++ test.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# Author: Oxnz
+
+http://cs.canisius.edu/ONLINESTUFF/PL_TUTORIALS/AWK/awk.examples
+awk 80 break lines
+function ee() {
+echo "a=$a"
+echo "b=$b"
+echo "c=$c"
+echo "endofee"
+}
+
+function te() {
+a=1 \
+	b=2 \
+	c=3 \
+	ee
+}
+
+te
+
+echo "a=$a"
+echo "b=$b"
+echo "c=$c"
+
+CENTOS:
+
+[0m[1;33m                   ..                    [0m[1;33mzhangpan05[0m[1m@[0m[0m[1;33mX230[0m
+[0m[1;33m                 .PLTJ.                 [0m[1;33m OS:[0m Ubuntu 14.04 trusty[0m
+[0m[1;33m                <><><><>                [0m[1;33m Kernel:[0m x86_64 Linux 3.13.0-34-generic[0m
+       [0m[1;32mKKSSV' 4KKK [0m[1;33mLJ[0m[1;35m KKKL.'VSSKK       [0m[1;33m Uptime:[0m 2h 36m[0m
+       [0m[1;32mKKV' 4KKKKK [0m[1;33mLJ[0m[1;35m KKKKAL 'VKK       [0m[1;33m Packages:[0m 1929[0m
+       [0m[1;32mV' ' 'VKKKK [0m[1;33mLJ[0m[1;35m KKKKV' ' 'V       [0m[1;33m Shell:[0m zsh 5.0.2[0m
+       [0m[1;32m.4MA.' 'VKK [0m[1;33mLJ[0m[1;35m KKV' '.4Mb.       [0m[1;33m Resolution:[0m 3286x1080[0m
+[0m[1;35m     . [0m[1;32mKKKKKA.' 'V [0m[1;33mLJ[0m[1;35m V' '.4KKKKK [0m[1;34m.     [0m[1;33m DE:[0m Unity 7.2.2[0m
+[0m[1;35m   .4D [0m[1;32mKKKKKKKA.'' [0m[1;33mLJ[0m[1;35m ''.4KKKKKKK [0m[1;34mFA.   [0m[1;33m WM:[0m Compiz[0m
+[0m[1;35m  <QDD ++++++++++++  [0m[1;34m++++++++++++ GFD>  [0m[1;33m WM Theme:[0m Ambiance[0m
+[0m[1;35m   'VD [0m[1;34mKKKKKKKK'.. [0m[1;32mLJ [0m[1;33m..'KKKKKKKK [0m[1;34mFV    [0m[1;33m GTK Theme:[0m Ambiance [GTK2], Ambiance [GTK3][0m
+[0m[1;35m     ' [0m[1;34mVKKKKK'. .4 [0m[1;32mLJ [0m[1;33mK. .'KKKKKV [0m[1;34m'     [0m[1;33m Icon Theme:[0m ubuntu-mono-dark[0m
+       [0m[1;34m 'VK'. .4KK [0m[1;32mLJ [0m[1;33mKKA. .'KV'        [0m[1;33m Font:[0m Ubuntu 11[0m
+       [0m[1;34mA. . .4KKKK [0m[1;32mLJ [0m[1;33mKKKKA. . .4       [0m[1;33m CPU:[0m Intel Core i5-3320M CPU @ 2.601GHz[0m
+       [0m[1;34mKKA. 'KKKKK [0m[1;32mLJ [0m[1;33mKKKKK' .4KK       [0m[1;33m RAM:[0m 2018MB / 3652MB[0m
+       [0m[1;34mKKSSA. VKKK [0m[1;32mLJ [0m[1;33mKKKV .4SSKK       [0m
+[0m[1;32m                <><><><>                 [0m
+[0m[1;32m                 'MKKM'                  [0m
+[0m[1;32m                   ''[0m
