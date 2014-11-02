4db71462985bbaf6abc867d9a0c371d7960509c6
oxnz <yunxinyi@gmail.com>
2014-11-02 17:53:54 +0800

    remove duplicate config

--- shell/functions.bash
+++ shell/functions.bash
@@ -6,7 +6,6 @@ function prompt_command() {
     GIT_PS1_SHOWDIRTYSTATE='Y' \
     GIT_PS1_SHOWSTASHSTATE='Y' \
     GIT_PS1_SHOWCOLORHINTS='Y' \
-    GIT_PS1_SHOWCOLORHINTS='Y' \
     GIT_PS1_SHOWUNTRACKEDFILES='Y' \
     __git_ps1 "(%s)"
 }
