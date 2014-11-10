e8a8b644e5684ba477c83ceb8ad552d90538e535
oxnz <yunxinyi@gmail.com>
2014-11-10 22:23:54 +0800

    add xterm 256 color adjust code

--- shell/switches.sh
+++ shell/switches.sh
@@ -1,5 +1,27 @@
 # Switches for shell control
 
+# Enable 256 color capabilities for appropriate terminals
+# ref: http://fedoraproject.org/wiki/Features/256_Color_Terminals#256_Color_Terminals
+#
+# Set this variable in your local shell config if you want remote
+# xterms connecting to this system, to be sent 256 colors.
+# This can be done in /etc/csh.cshrc, or in an earlier profile.d script.
+#   SEND_256_COLORS_TO_REMOTE=1
+
+# Terminals with any of the following set, support 256 colors (and are local)
+if [ -n "$COLORTERM$XTERM_VERSION$ROXTERM_ID$KONSOLE_DBUS_SESSION" ]; then
+    case $TERM in
+        xterm) TERM=xterm-256color;;
+        screen) TERM=screen-256color;;
+        Eterm) TERM=Eterm-256color;;
+    esac
+    export TERM
+    if [ -n "$TERMCAP" ] && [ "$TERM" = "screen-256color" ]; then
+        TERMCAP=$(echo "$TERMCAP" | sed -e 's/Co#8/Co#256/g')
+        export TERMCAP
+    fi
+fi
+
 export CLICOLOR=1
 export LC_ALL=en_US.UTF-8
 
