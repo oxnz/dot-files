b082f7b5721dc078c8d3f61821a4b6db5dcd54a9
max_zhang <max_zhang@max-z-mac.local>
2021-08-05 17:58:56 +0800

    FEATURE-install

---
+++ install.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# [git]
+# install git hooks
+
+install -dv ~/.git/hooks
+install -bCSv git/hooks/* ~/.git/hooks
+
+# install gitignore
+install -bCSv gitignore ~/.gitignore
+
+# install gitconfig
+sh gitconfig
+
+# [vim]
+
+install -dv ~/.vim/doc
+install -bCSv vim/doc/* ~/.vim/doc
+install -dv ~/.vim/plugin
+install -bCSv vim/plugin/* ~/.vim/plugin
+install -dv ~/.vim/templates
+install -bCSv vim/templates/* ~/.vim/templates
+
+install -bCSv vimrc ~/.vimrc
+
+# [bash]
+install -bCSv bashrc ~/.bashrc
+
+# [zsh]
+install -bCSv zshrc ~/.zshrc
+
+# [pythonrc]
+install -bCSv pythonrc ~/.pythonrc
