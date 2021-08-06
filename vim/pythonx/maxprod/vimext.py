99271f54859c00c4855f7e7c93e173fadce9d344
max_zhang <max_zhang@max-z-mac>
2021-08-06 19:44:20 +0800

    [feature] vim add pythonx as module path, revise install.sh
    1. add vim/pythonx/maxprod/vimext.py as a placeholder
    2. add install vim/pythonx code -> install.sh
    3. modify vimrc to load python2 prior to python3

---
+++ vim/pythonx/maxprod/vimext.py
@@ -0,0 +1,15 @@
+from __future__ import (absolute_import, division,
+                        print_function, unicode_literals)
+from builtins import *
+from future.standard_library import install_aliases
+install_aliases()
+
+import vim
+import sys
+
+
+def proc():
+    pass
+
+if __name__ == '__main__':
+    print(sys.argv)
